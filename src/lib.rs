f41161a7eda0e993173af6c9b788e17117346b0e
krdlab <krdlab@gmail.com>
2022-03-30 17:32:49 +0900

    feat: add BlockId

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+// Copyright (c) 2022 Sho Kuroda <krdlab@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct BlockId {
+    filename: String,
+    blknum: i32,
+}
+
+impl BlockId {
+    pub fn new(filename: &str, blknum: i32) -> Self {
+        BlockId {
+            filename: filename.to_owned(),
+            blknum,
+        }
+    }
+
+    pub fn filename(&self) -> &str {
+        &self.filename
+    }
+
+    pub fn number(&self) -> i32 {
+        self.blknum
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        let result = BlockId::new("test", 1);
+        assert_eq!(result.filename(), "test");
+        assert_eq!(result.number(), 1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+// Copyright (c) 2022 Sho Kuroda <krdlab@gmail.com>
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+use simpledb::BlockId;
+
 fn main() {
-    println!("Hello, world!");
+    let b = BlockId::new("", 0);
+    println!("{:?}", b);
 }
